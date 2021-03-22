705df63e1fab4e61dd7b979797d592cbe170a604
Akshay Grover <akshaygrover588@gmail.com>
2021-03-22 15:17:23 -0400

    Initial Commit

---
+++ REDspace Code Challenge/REDspace_Code_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  REDspace_Code_ChallengeApp.swift
+//  REDspace Code Challenge
+//
+//  Created by Akshay Grover on 2021-03-22.
+//
+
+import SwiftUI
+
+@main
+struct REDspace_Code_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
